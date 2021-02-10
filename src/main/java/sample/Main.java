61db38624257e6d2354483136f67bcfab131f713
Guyliane <guyliane@live.fr>
2021-02-10 11:43:50 +0100

    Ajout de MAVEN au projet afin d'utiliser hibernate et d'autre dependance par la suite

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -14,7 +14,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/sample/view/login.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getResource("/sample/view/login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("login.fxml"));
         primaryStage.setTitle("Hello World");
         Scene scene = new Scene(root,700,400);
         primaryStage.setScene(scene);
